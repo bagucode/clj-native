648d765ecb210ea941c58f69bbc2c79f0d683a16
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2010-01-07 21:58:26 +0100

    apparently I had not saved core.clj

--- src/clj_native/core.clj
+++ src/clj_native/core.clj
@@ -1,3 +1,11 @@
+;;   Copyright (c) Markus Gustavsson. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;   which can be found in the file epl-v10.html at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
 (ns clj-native.core
   (:use [clojure.contrib.seq-utils :only [indexed]])
   (:import [clojure.asm ClassVisitor MethodVisitor
