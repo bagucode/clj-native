5d275d69087de040b897c68e78434119e3d1ba99
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2010-01-07 21:32:26 +0100

    lein-swank 1.1.0

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  ;; Could not find a repo that had the latest jna version
                  ;; so this is a local install (put on clojars?)
                  [com.sun.jna/jna "3.2.4"]]
-  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+
