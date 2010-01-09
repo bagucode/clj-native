7e3e52ec278f734f6b39fd935c2644bb831b478c
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2010-01-09 14:26:22 +0100

    using clojars version of jna

--- project.clj
+++ project.clj
@@ -2,8 +2,6 @@
   :description "Simplify usage of native libs from Clojure. Uses JNA."
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 ;; Could not find a repo that had the latest jna version
-                 ;; so this is a local install (put on clojars?)
-                 [com.sun.jna/jna "3.2.4"]]
+                 [org.clojars.bagucode/jna "3.2.4"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
 
