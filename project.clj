997459b3670a68c94cb0cbe393b22fc798f4c5db
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2010-03-07 15:07:08 +0100

    version bump for interface change

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject clj-native "0.6.2-SNAPSHOT"
+(defproject clj-native "0.7.0-SNAPSHOT"
   :description "Simplify usage of native libs from Clojure. Uses JNA."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [org.clojars.bagucode/jna "3.2.4"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
-
