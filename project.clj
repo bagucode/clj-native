aa029e67e8f7eb0ed47cfaf5b7c11918bfd2f2cc
Markus Gustavsson <markus.gustavsson@gmail.com>
2012-11-13 10:04:39 +0100

    overdue version bump

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject clj-native "0.9.2-SNAPSHOT"
+(defproject clj-native "0.9.3-SNAPSHOT"
   :description "Simplify usage of native libs from Clojure. Uses JNA."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.java.dev.jna/jna "3.4.0"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.4.2"]])
+                 [net.java.dev.jna/jna "3.4.0"]])
