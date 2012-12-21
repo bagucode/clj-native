7319b296797892ce344aa5e8b9480d49623f075b
Markus Gustavsson <markus.gustavsson@gmail.com>
2012-12-21 09:33:30 +0100

    Remove -SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-native "0.9.3-SNAPSHOT"
+(defproject clj-native "0.9.3"
   :description "Simplify usage of native libs from Clojure. Uses JNA."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.java.dev.jna/jna "3.4.0"]])
